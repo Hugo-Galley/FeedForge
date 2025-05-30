aef0e4635af099449434d66c2712aa0f023a9347
Freaks <galleyhugo@icloud.com>
2025-05-30 18:25:33 +0200

    Ajout du scraaping des lien de flux RSS

--- backend/bdd/bdd.sql
+++ backend/bdd/bdd.sql
@@ -1,26 +1,31 @@
 CREATE DATABASE `FeedForge`;
-USE `FeedForge`
+USE `FeedForge`;
 
 -- Une table User classique
 CREATE TABLE Users (
     userId CHAR(36) NOT NULL PRIMARY KEY,
     username NOT NULL VARCHAR(255),
     email NOT NULL VARCHAR(255),
-    passwd NOT NULL TEXT
+    passwd NOT NULL TEXT,
     profilPicture NOT NULL TEXT
-)
+);
+
 -- une table pour stocker les flux RSS avec les infos (scrapper le site de flux dis dans notion)
 CREATE TABLE RssFlowLibrary(
     rssFlowLibraryId NOT NULL CHAR(36) PRIMARY KEY,
     flowName NOT NULL VARCHAR(255),
     flowLink NOT NULL TEXT,
     category NOT NULL VARCHAR(255),
-)
+    logo TEXT NOT NULL,
+    domains TEXT NOT NULL
+);
+
 -- Une table pour stocker les identifiant de chaine youtube associé a leur nom affin d'evité les appel API trop couteux
 CREATE TABLE YoutubeChannelId(
-    name NOT NULL PRIMARY KEY
+    name NOT NULL PRIMARY KEY,
     channelId TEXT NOT NULL
-)
+);
+
 -- une table pour stocker les infos lié au flux de la personne (Article vidéo, etc)
 CREATE TABLE CustomRssFlow(
     customRssFlowId CHAR(36) NOT NULL PRIMARY KEY,
@@ -28,9 +33,9 @@ CREATE TABLE CustomRssFlow(
     articlePublicationDate NOT NULL TEXT,
     articleLink NOT NULL TEXT,
     articleDescription NOT NULL TEXT,
-    articleLanguage NOT NULL VARCHAR(30)
+    articleLanguage NOT NULL VARCHAR(30),
     rssFlowLibrairyId NOT NULL CHAR(36),
-    userId NOT NULL CHAR(36)
-    FOREIGN KEY rssFlowLibrairyId REFERENCES RssFlowLibrary(rssFlowLibraryId) ON DELETE CASCADE,
-    FOREIGN KEY userId REFERENCES Users(userId) ON DELETE CASCADE
-)
+    userId NOT NULL CHAR(36),
+    FOREIGN KEY (rssFlowLibrairyId) REFERENCES RssFlowLibrary(rssFlowLibraryId) ON DELETE CASCADE,
+    FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE
+);
---
+++ backend/scripts/scraping.py
@@ -0,0 +1,48 @@
+import time
+import uuid
+import requests
+from bs4 import BeautifulSoup
+import re
+import xml.etree.ElementTree as ET
+from config import db
+from models import RssFlowLibrary
+
+def scrappRssFlow(xmlFileurl,category):
+    atlasRssUrl = f"https://atlasflux.saynete.net/{xmlFileurl}"
+    response = requests.get(atlasRssUrl)
+    if response.status_code == 200:
+        root = ET.fromstring(response.text)
+        for flux in root.findall("flux"):
+            domains = re.findall(r"^https?:\/\/([^\/]+)",flux.find("adresse").text)
+            newRssFlux = RssFlowLibrary(
+                rssFlowLibraryId = uuid.uuid4(),
+                flowName = flux.find("source").text,
+                flowLink = flux.find("adresse").text,
+                category = category,
+                domains = domains[0],
+                logo = f"https://logo.clearbit.com/{domains[0]}"
+            )
+            db.add(newRssFlux)
+            db.commit()
+            db.refresh(newRssFlux)
+def scrapRssCategory():
+    url = "https://atlasflux.saynete.net"
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text,"html.parser")
+        ongleTheme = soup.select_one("#onglet_theme")
+
+        categorys = ongleTheme.find_all(
+            lambda tag: tag.has_attr("class") and any(c in ["elem_onss", "elem_onc","elem_ongl"] for c in tag["class"])
+        )
+        for category in categorys:
+            categoryName = category.get_text(strip=True)
+            onclick = category.get("onclick", "")
+            match = re.search(r"'([^']+\.xml)'", onclick)
+            fichier_xml = match.group(1) if match else "N/A"
+            scrappRssFlow(fichier_xml,categoryName)
+
+startTime = time.time()
+scrapRssCategory()
+endTime = time.time()
+print(f"Il aura fallu : {endTime - startTime} secondes")
